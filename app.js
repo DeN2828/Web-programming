a252110914322349eaf250f10e1fb8e34e7b9f5a
DeN2828 <den-tu@ua.fm>
2023-11-19 14:12:30 +0200

    hm9 find count symbol with help altcode

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-const Buffer = require('buffer').Buffer;
-const buffer = 
-[
-[ 84, 4 ],
-[ 67, 4 ],
-[ 54, 4, 56, 4, 66, 4, 66, 4, 86, 4 ],
-[ 59, 4, 78, 4, 52, 4, 56, 4, 61, 4, 56, 4],
-[ 64, 4, 62, 4, 55, 4, 67, 4, 60, 4, 61, 4, 62, 4, 87, 4 ],
-[ 18, 4, 56, 4, 50, 4, 71, 4, 53, 4, 61, 4, 61, 4, 79, 4 ],
-[ 61, 4, 48, 4, 52, 4, 55, 4, 50, 4, 56, 4, 71, 4, 48,
-4, 57, 4, 61, 4, 62, 4],
-[ 63, 4, 64, 4, 62, 4, 51, 4, 64, 4, 48, 4, 60, 4, 67, 4, 50, 4,
-48, 4, 61, 4, 61, 4, 79, 4 ],
-[ 50, 4, 48, 4, 54, 4, 59, 4, 56, 4, 50, 4, 56, 4, 60, 4 ]
-];
+const fs = require('fs');
+const args = process.argv.slice(2);
 
-// 1. Створити з даних цих масивів масив буферів різного розміру.
-const bufferarray = buffer.map(buffer => Buffer.from(buffer));
-console.log(bufferarray);
-// 2. Визначити вірне кодування для кожного буферу (лише два варіанти, див.Вище)
-const findencoding = bufferarray.map(buffer => buffer.includes(0) ? 'utf8' : 'utf16le');
-console.log(findencoding);
-// 3. Перекодувати кожен буфер у читабельну стрічку, яка є словом.
+if(args.length !== 2)
+{
+  console.log("Wrong! Use 2 arguments <file_path> and <symbol_code>");
+}
 
-// Функція для перекодування буфера в читабельний рядок
-function decodeBuffer(buffer, encoding) {
-    return Buffer.from(buffer).toString(encoding);
-  }
-  const decodedwords = bufferarray.map((buffer, index) => decodeBuffer(buffer, findencoding[index]));
-console.log(decodedwords);
+else
+{
+  const filepath = args[0];
+  const symbolcode = parseInt(args[1]);
 
-// 4. Вивести у консоль отримані слова у логічному порядку щоб скласти таємне посилання від викладача до студентів.
-const groupedSentences = decodedwords[5].concat(' ', decodedwords[7]).concat(' ',decodedwords[0]).concat(' ',decodedwords[6])
-.concat(' ',decodedwords[8]).concat(' ',decodedwords[1]).concat(' ',decodedwords[2]).concat(' ',decodedwords[4])
-.concat(' ',decodedwords[3])
-console.log(groupedSentences);
+  fs.readFile(filepath, (err,data) =>
+  {
+    if(err){console.error(`Error reading file: ${err.message}`)}
+
+    else
+    {
+      let count = 0;
+
+      for(const byte of data)
+      {
+        if(byte === symbolcode)
+        {
+          count++;
+        }
+      }
+      // for(const byte in data)
+      // {
+      //   if(byte === symbolcode)
+      //   {
+      //     count++;
+      //   }
+      // }
+      console.log(`The symbol ${symbolcode} was found in file ${filepath} - ${count} times`);
+    }
+  });
+}
