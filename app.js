743dfeed39f56bee3f131c8099a194e150019a15
DeN2828 <den-tu@ua.fm>
2023-11-26 21:30:41 +0200

    express routes

--- app.js
+++ app.js
@@ -1,39 +1,87 @@
-const fs = require('fs');
-const args = process.argv.slice(2);
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyparser = require('body-parser');
+let students =
+  [
+    { id: 10, firstName: "Marty", lastName: "McFly", group: 101, rate: 9.5 },
+    { id: 11, firstName: "Squidward", lastName: "Tentakles", group: 102, rate: 6.1 },
+    { id: 12, firstName: "Donald", lastName: "Duck", group: 102, rate: 7.2 },
+    { id: 13, firstName: "Sarah", lastName: "Connor", group: 101, rate: 8.3 },
+    { id: 14, firstName: "Yugin", lastName: "Krabbs", group: 102, rate: 6.8 },
+  ];
 
-if(args.length !== 2)
-{
-  console.log("Wrong! Use 2 arguments <file_path> and <symbol_code>");
-}
+  app.use(bodyparser.json());
 
-else
-{
-  const filepath = args[0];
-  const symbolcode = parseInt(args[1]);
+//GET all students
+app.get('/students', (req, res) => {
+  res.json(students);
+});
 
-  fs.readFile(filepath, (err,data) =>
-  {
-    if(err){console.error(`Error reading file: ${err.message}`)}
+//POST new students
+app.post('/students/:id', (req, res) => {
+  // res.sendFile(path.join(__dirname + '/index.html'));
+  console.log(req.query,req.params,req.body);
+  const { id } = req.params;
+  const { firstName, lastName, group, rate } = req.body;
 
-    else
-    {
-      let count = 0;
+  // Check if firstName and lastName are empty
+  if (!firstName || !lastName) {
+    return res.status(400).json({ error: 'First Name and Last Name is empty' });
+  }
 
-      for(const byte of data)
-      {
-        if(byte === symbolcode)
-        {
-          count++;
-        }
-      }
-      // for(const byte in data)
-      // {
-      //   if(byte === symbolcode)
-      //   {
-      //     count++;
-      //   }
-      // }
-      console.log(`The symbol ${symbolcode} was found in file ${filepath} - ${count} times`);
-    }
-  });
-}
+  const newStudent = { id: parseInt(id), firstName: firstName, lastName: lastName, group: group, rate: rate };
+  students.push(newStudent);
+  res.json(newStudent);
+});
+
+//GET students by id
+app.get('/students/:id', (req, res) => {
+  const { id } = req.params;
+  const student = students.find(s => s.id === parseInt(id));
+
+  // Check if id not found
+  if (!student) { res.status(404).json({ error: 'Student not found' }) };
+
+  res.json(student);
+
+});
+
+//PUT students by id
+app.put('/students/:id', (req, res) => {
+  const id = req.params.id;
+  const idd = req.params;
+  console.log(id,idd);
+  const student = students.find(s => s.id === parseInt(id));
+
+  // Check if id not found
+  if (!student) { res.status(404).json({ error: 'Student not found' }) }
+  else {
+    const { firstName, lastName, group, rate } = req.body;
+    students[student] = { id: parseInt(id), firstName, lastName, group, rate };
+  }
+
+  res.json(students[student]);
+
+});
+
+// DELETE student by id
+app.delete('/students/:id', (req, res) => {
+  const { id } = req.params;
+  const index = students.findIndex(s => s.id === parseInt(id));
+
+  if (index === -1) {
+    return res.status(404).json({ error: 'Student not found.' });
+  }
+
+  const deletedStudent = students.splice(index, 1)[0];
+  res.json(deletedStudent);
+});
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
